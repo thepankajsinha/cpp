391f74445cc24109fa60669b50d2ca7cc4205ac1
Pankaj Sinha <136917326+thepankajsinha@users.noreply.github.com>
2024-10-06 06:11:03 +0530

    Add files via upload

---
+++ String/StringOutput.cpp
@@ -0,0 +1,15 @@
+//Strings are used for storing text/characters.
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Create string
+    string greet = "Good Morning";
+
+    //update string
+    greet = "Good Evening";
+
+    //print output
+    cout<<greet;
+}
+//Good Evening
